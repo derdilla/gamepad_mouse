4ccad7ed90293d4a6ff247730fe4987c290fe8e4
derdilla <82763757+derdilla@users.noreply.github.com>
2025-01-22 19:18:42 +0100

    refactor: rewrite to be more compatible (#1)
    * refactor: rewrite to be more compatible and faster, using modern dependencies
    
    * fix excessive ressource use
    
    * move mouse speed modifier complexity to mouse handler

---
+++ src/main.rs
@@ -0,0 +1,135 @@
+mod mouse;
+
+use std::{thread, time::Duration};
+
+use dialoguer::FuzzySelect;
+use enigo::{Enigo, Keyboard, Mouse, Settings};
+use gilrs::{Event, EventType::*, Gamepad, GamepadId, Gilrs};
+
+use mouse::*;
+
+fn select_gamepad(gilrs: &Gilrs) -> Option<(GamepadId, Gamepad)> {
+    let gamepads: Vec<(GamepadId, Gamepad<'_>)> = gilrs.gamepads().collect();
+
+    if gamepads.is_empty() {
+        println!("Couldn't detect any gamepads!");
+        None
+    } else if gamepads.len() == 1 {
+        gamepads.first().map(|e| e.clone())
+    } else {
+        let gamepad_names = gamepads
+            .iter()
+            .map(|(id, pad)| format!("{} {}", &id, &pad.name()));
+        let selection = FuzzySelect::new()
+            .with_prompt("Multiple gamepads available:")
+            .items(&gamepad_names.collect::<Vec<String>>())
+            .interact()
+            .unwrap();
+        gamepads.get(selection).map(|e| e.clone())
+    }
+}
+
+struct ButtonMapper {
+    enigo: Enigo,
+    mouse_mover: MouseMoverHandle,
+    // store the offset of the last knob movement to fix some controllers not returning
+    // a definite zero value when letting go.
+    last_x: f32,
+    last_y: f32,
+}
+
+impl ButtonMapper {
+    pub fn new() -> Self {
+        let mouse_mover = MouseMoverHandle::get();
+        ButtonMapper {
+            enigo: Enigo::new(&Settings::default()).unwrap(),
+            mouse_mover,
+            last_x: 0.0,
+            last_y: 0.0,
+        }
+    }
+
+    #[rustfmt::skip]
+    pub fn handle_event(&mut self, event: gilrs::EventType) {
+        let performed_action = match event {
+            // enter (ABXY on gamepad)
+            ButtonPressed(gilrs::Button::South, _) => self.enigo.key(enigo::Key::Return, enigo::Direction::Press),
+            ButtonReleased(gilrs::Button::South, _) => self.enigo.key(enigo::Key::Return, enigo::Direction::Release),
+            ButtonPressed(gilrs::Button::East | gilrs::Button::North | gilrs::Button::West, _) => Ok(()),
+            ButtonReleased(gilrs::Button::East | gilrs::Button::North | gilrs::Button::West, _) => Ok(()),
+
+            // arrow keys
+            ButtonPressed(gilrs::Button::DPadUp, _) => self.enigo.key(enigo::Key::UpArrow, enigo::Direction::Press),
+            ButtonReleased(gilrs::Button::DPadUp, _) => self.enigo.key(enigo::Key::UpArrow, enigo::Direction::Release),
+            ButtonPressed(gilrs::Button::DPadRight, _) => self.enigo.key(enigo::Key::RightArrow, enigo::Direction::Press),
+            ButtonReleased(gilrs::Button::DPadRight, _) => self.enigo.key(enigo::Key::RightArrow, enigo::Direction::Release),
+            ButtonPressed(gilrs::Button::DPadDown, _) => self.enigo.key(enigo::Key::DownArrow, enigo::Direction::Press),
+            ButtonReleased(gilrs::Button::DPadDown, _) => self.enigo.key(enigo::Key::DownArrow, enigo::Direction::Release),
+            ButtonPressed(gilrs::Button::DPadLeft, _) => self.enigo.key(enigo::Key::LeftArrow, enigo::Direction::Press),
+            ButtonReleased(gilrs::Button::DPadLeft, _) => self.enigo.key(enigo::Key::LeftArrow, enigo::Direction::Release),
+            ButtonPressed(gilrs::Button::Start, _) => self.enigo.key(enigo::Key::Meta, enigo::Direction::Press),
+
+            // central keys
+            ButtonReleased(gilrs::Button::Start, _) => self.enigo.key(enigo::Key::Meta, enigo::Direction::Release),
+            
+            // mouse (trigger is the small button trigger2 the big, lower one)
+            ButtonChanged(gilrs::Button::LeftTrigger, v, _) => self.enigo.button(enigo::Button::Back, Self::dir(v)),
+            ButtonChanged(gilrs::Button::LeftTrigger2, v, _) => self.enigo.button(enigo::Button::Left, Self::dir(v)),
+            ButtonChanged(gilrs::Button::RightTrigger, v, _) => self.enigo.button(enigo::Button::Forward, Self::dir(v)),
+            ButtonChanged(gilrs::Button::RightTrigger2, v, _) => self.enigo.button(enigo::Button::Right, Self::dir(v)),
+
+            // mouse movement
+            AxisChanged(gilrs::Axis::LeftStickX, mut v, _) => {
+                if (v.abs() < 0.010)
+                    || (v.abs() < 0.04 && v.abs() < self.last_x.abs()) {
+                    v = 0.0;
+                }
+                self.last_x = v;
+                self.mouse_mover.set_target_vel_x(v); Ok(()) },
+            AxisChanged(gilrs::Axis::LeftStickY, mut v, _) => {
+                if (v.abs() < 0.015)
+                    || (v.abs() < 0.05 && v.abs() < self.last_y.abs()) {
+                    v = 0.0;
+                }
+                self.last_y = v;
+                self.mouse_mover.set_target_vel_y(v); Ok(()) },
+            _ => Ok(()),
+        };
+        if let Err(err) = performed_action {
+            println!("Failed to simulate action: {:?}", err);
+        }
+    }
+
+    /// Determine whether to press or release from button presse strenght (0-1).
+    fn dir(v: f32) -> enigo::Direction {
+        assert!((0.0..=1.0).contains(&v));
+        if v >= 0.5 {
+            enigo::Direction::Press
+        } else {
+            enigo::Direction::Release
+        }
+    }
+}
+
+fn main() {
+    let mut gilrs = Gilrs::new().unwrap();
+    if let Some((gamepadid, gamepad)) = select_gamepad(&gilrs) {
+        println!("Selected: {}", &gamepad.name());
+        let mut mapper = ButtonMapper::new();
+        loop {
+            //println!("{:?}", &gamepad);
+            while let Some(Event {
+                id, event, ..
+            }) = gilrs.next_event()
+            {
+                if id != gamepadid {
+                    continue;
+                }
+                mapper.handle_event(event);
+
+                //println!("{:?} New event: {:?}", time, event);
+            }
+            thread::sleep(Duration::from_millis(100))
+        }
+    }
+}
---
+++ src/mouse.rs
@@ -0,0 +1,149 @@
+use std::{
+    sync::mpsc::{self, Receiver, Sender},
+    thread,
+    time::{Duration, SystemTime},
+};
+
+use enigo::{Enigo, Mouse};
+
+#[derive(Debug)]
+struct MouseMover {
+    max_change_per_milli: f32,
+    mouse_speed_mod: f32,
+    last_tick: SystemTime,
+    target_vel_x: f32,
+    current_vel_x: f32,
+    target_vel_y: f32,
+    current_vel_y: f32,
+    enigo: Enigo,
+}
+
+impl MouseMover {
+    pub fn new() -> MouseMover {
+        MouseMover {
+            // whatever constant feels good.
+            max_change_per_milli: 0.3,
+            mouse_speed_mod: 2.0,
+            last_tick: SystemTime::now(),
+            target_vel_x: 0.0,
+            current_vel_x: 0.0,
+            target_vel_y: 0.0,
+            current_vel_y: 0.0,
+            enigo: Enigo::new(&enigo::Settings::default()).unwrap(),
+        }
+    }
+
+    pub fn start(mut self) -> MouseMoverHandle {
+        let (handler, recv) = MouseMoverHandle::new();
+        thread::spawn(move || {
+            loop {
+                if let Ok((x, y)) = recv.recv_timeout(Duration::from_nanos(1000)) {
+                    self.target_vel_x = x * self.mouse_speed_mod;
+                    self.target_vel_y = y * self.mouse_speed_mod;
+                }
+                self.tick();
+                thread::sleep(Duration::from_millis(1));
+
+                if self.target_vel_x == 0.0
+                    && self.current_vel_x == 0.0
+                    && self.target_vel_y == 0.0
+                    && self.current_vel_y == 0.0
+                {
+                    // Wait for next message
+                    match recv.recv() {
+                        Ok((x, y)) => {
+                            self.target_vel_x = x;
+                            self.target_vel_y = y;
+                        },
+                        Err(_) => break,
+                    }
+                }
+            }
+        });
+        handler
+    }
+
+    fn tick(&mut self) {
+        if self.target_vel_x == 0.0
+            && self.current_vel_x == 0.0
+            && self.target_vel_y == 0.0
+            && self.current_vel_y == 0.0
+        {
+            self.last_tick = SystemTime::now();
+            return;
+        }
+
+        let now = SystemTime::now();
+        let elapse_millis = now
+            .duration_since(self.last_tick)
+            .unwrap_or(Duration::ZERO)
+            .as_millis();
+        let max_change =
+            (((self.max_change_per_milli as f64) * (elapse_millis as f64)) as f32).abs();
+
+        // Calculate a factor no higher than [max_change_per_nanno] to get from [curr] to [target].
+        self.current_vel_x += (self.target_vel_x - self.current_vel_x)
+            .min(max_change)
+            .max(-1.0 * max_change);
+        self.current_vel_y += (self.target_vel_y - self.current_vel_y)
+            .min(max_change)
+            .max(-1.0 * max_change);
+
+        // hack for faster slowdowns
+        if self.target_vel_x == 0.0 {
+            self.current_vel_x /= 2.0;
+        }
+        if self.target_vel_y == 0.0 {
+            self.current_vel_y /= 2.0;
+        }
+
+        self.last_tick = now;
+        self.enigo
+            .move_mouse(
+                self.current_vel_x.round() as i32,
+                self.current_vel_y.round() as i32,
+                enigo::Coordinate::Rel,
+            )
+            .expect("Unable to move mouse.");
+    }
+}
+
+pub struct MouseMoverHandle {
+    last_x: f32,
+    last_y: f32,
+    sender: Sender<(f32, f32)>,
+}
+
+impl MouseMoverHandle {
+    pub fn get() -> Self {
+        MouseMover::new().start()
+    }
+
+    pub fn new() -> (Self, Receiver<(f32, f32)>) {
+        let (send, rec) = mpsc::channel();
+        (
+            MouseMoverHandle {
+                last_x: 0.0,
+                last_y: 0.0,
+                sender: send,
+            },
+            rec,
+        )
+    }
+
+    pub fn set_target_vel_x(&mut self, x: f32) {
+        self.last_x = x;
+        self.send();
+    }
+
+    pub fn set_target_vel_y(&mut self, y: f32) {
+        self.last_y = y;
+        self.send();
+    }
+
+    fn send(&mut self) {
+        self.sender
+            .send((self.last_x, self.last_y))
+            .expect("process ended unexpectedly");
+    }
+}
